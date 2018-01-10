2bd01c5fed7a0125c3c24a8fbc3280d0990d8056
siosio <siosio12@gmail.com>
2018-01-10 12:56:01 +0900

    http proxy対応出来ていなかったので対応

--- src/main/java/siosio/Client.kt
+++ src/main/java/siosio/Client.kt
@@ -1,13 +1,26 @@
 package siosio
 
+import com.intellij.util.net.*
+import org.apache.http.client.config.*
 import org.apache.http.client.methods.*
 import org.apache.http.impl.client.*
+import java.util.concurrent.*
 
 object Client {
 
     fun get(uri: String): String {
-        val response = HttpClients.createDefault()
-                .execute(HttpGet(uri))
+        val clientBuilder = RequestConfig.custom()
+        IdeHttpClientHelpers.ApacheHttpClient4.setProxyForUrlIfEnabled(clientBuilder, uri)
+
+        val response: CloseableHttpResponse = try {
+            HttpClients.custom()
+                    .setDefaultRequestConfig(clientBuilder.build())
+                    .setConnectionTimeToLive(1, TimeUnit.SECONDS)
+                    .build()
+                    .execute(HttpGet(uri))
+        } catch (e: Exception) {
+            return ""
+        }
 
         return when (response.statusLine.statusCode) {
             200 -> response.entity.content.reader().readText()
