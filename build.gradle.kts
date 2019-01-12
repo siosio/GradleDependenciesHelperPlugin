5f5e772a2e5b931f200812d5763c0e4a704ccbbf
siosio <siosio12@gmail.com>
2019-01-12 18:51:27 +0900

    change build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
+    }
+}
+
+plugins {
+    id("org.jetbrains.intellij") version "0.4.1"
+}
+
+apply(plugin = "java")
+apply(plugin = "kotlin")
+
+version = "1.13"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.11")
+    testImplementation("junit:junit:4.12")
+}
+
+
+intellij {
+    pluginName = "Gradle Dependencies Helper"
+    version = "IC-2018.3.3"
+    updateSinceUntilBuild = false
+    setPlugins("Groovy", "gradle")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "5.1.1"
+    }
+}
